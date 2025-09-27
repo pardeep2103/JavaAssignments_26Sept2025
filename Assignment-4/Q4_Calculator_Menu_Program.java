c48f1eb0b9433f607ea843fce82d64e8426573e8
pardeep2103 <pardeepgoswami1807s@gmail.com>
2025-09-27 23:24:17 +0530

    Update Q4_Calculator_Menu_Program.java

--- Assignment-4/Q4_Calculator_Menu_Program.java
+++ Assignment-4/Q4_Calculator_Menu_Program.java
@@ -1,1 +1,43 @@
+package Assignment5;
+import java.util.Scanner;
 
+public class Q4_Calculator_Menu_Program {
+    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	       
+	        System.out.print("Enter first number: ");
+	        int a = sc.nextInt();
+	        System.out.print("Enter Second number: ");
+            int b = sc.nextInt();
+	        
+	        System.out.println("1) Addition");
+	        System.out.println("2) Subtraction");
+	        System.out.println("3) Multiplication");
+	        System.out.println("4) Division");
+	        System.out.println("5) Exit");
+	        
+	        System.out.print("Enter your choice : ");
+	        int c = sc.nextInt();
+	        
+	        if(c == 1) {
+	            System.out.println("The output is " + (a + b));
+	        }
+	        else if(c == 2) {
+	            System.out.println("The output is " + (a - b));
+	        }
+	        else if(c == 3) {
+	            System.out.println("The output is " + (a * b));
+	        }
+	        else if(c == 4) {
+	            System.out.println("The output is " + (a / b));
+	        }
+	        else if(c == 5) {
+	            System.out.println("Exit");
+	        }
+	        else {
+	            System.out.println("Wrong choice");
+	        }
+	        
+	        sc.close();
+	    }
+	}
