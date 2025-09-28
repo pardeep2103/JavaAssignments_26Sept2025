563636647b2aef4501ad08a4ee380ea7882ea6ec
pardeep2103 <pardeepgoswami1807s@gmail.com>
2025-09-28 18:54:54 +0530

    Update Q5_Employee_Class_System.java

--- Assignment-5/Q5_Employee_Class_System.java
+++ Assignment-5/Q5_Employee_Class_System.java
@@ -7,7 +7,7 @@ String Emp_name;
        int salary;
        String location;
        
-       public employee(String name, int emp_id, int emp_salary, String emp_location ) {
+       public Q5_Employee_Class_System(String name, int emp_id, int emp_salary, String emp_location ) {
           this.Emp_name=name;
           this.empid=emp_id;
           this.salary=emp_salary;
